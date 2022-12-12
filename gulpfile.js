bc8abd7bccdea0ec91b75ff1f8d3686d04deb0c1
Matthew Sirois <95838469+mjsirois@users.noreply.github.com>
2022-12-11 22:05:49 -0500

    Configured lintCSS module

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 const { src, dest, series, watch } = require(`gulp`),
-    htmlValidator = require(`gulp-html`);
+    htmlValidator = require(`gulp-html`),
+    CSSLinter = require(`gulp-stylelint`);
 
 let validateHTML = () => {
     return src([`index.html`])
         .pipe(htmlValidator(undefined));
 };
 
+let lintCSS = () => {
+    return src(`styles/main.css`)
+        .pipe(CSSLinter({
+            failAfterError: false,
+            reporters: [
+                {formatter: `string`, console: true}
+            ]
+        }));
+};
+
 exports.validateHTML = validateHTML;
+exports.lintCSS = lintCSS;
 
 
 
